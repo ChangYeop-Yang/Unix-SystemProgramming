557c9a2aee210ab011c51c5713e6ae07f61d2b5f
양창엽 <ChangYeop-Yang@users.noreply.github.com>
2017-11-28 21:39:05 +0900

    Init Commit
    Server and Client Commit

--- Client.c
+++ Client.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> 
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
+int main(int argc, char *argv[])
+{
+	int sock;
+   struct sockaddr_in serv_addr;
+   if (argc != 3) {
+      printf("Usage : %s <IP> <port>\n", argv[0]);
+      exit(1);
+   }
+
+   sock = socket(PF_INET, SOCK_STREAM, 0);
+
+   memset(&serv_addr, 0, sizeof(serv_addr));
+   serv_addr.sin_family = AF_INET;
+   serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+   serv_addr.sin_port = htons(atoi(argv[2]));
+
+   if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+      error_handling("connect() error");
+
+   close(sock);
+
+	return 0;
+}
