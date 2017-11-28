a38081bf8e8975af6100ab4cab1d82635acc99d7
YangHyeona <33485741+YangHyeona@users.noreply.github.com>
2017-11-28 22:26:02 +0900

    [Client] error_handling
    indentation(들여쓰기)과 connect()에서
     사용할 error_handling 함수 추가

--- Client.c
+++ Client.c
@@ -5,26 +5,39 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
+void error_handling(char * msg);
+
 int main(int argc, char *argv[])
 {
 	int sock;
-   struct sockaddr_in serv_addr;
-   if (argc != 3) {
-      printf("Usage : %s <IP> <port>\n", argv[0]);
-      exit(1);
-   }
+	struct sockaddr_in serv_addr;
+	if (argc != 3) {
+		printf("Usage : %s <IP> <port>\n", argv[0]);
+		exit(1);
+	}
 
-   sock = socket(PF_INET, SOCK_STREAM, 0);
+	//socket
+	sock = socket(PF_INET, SOCK_STREAM, 0);
 
-   memset(&serv_addr, 0, sizeof(serv_addr));
-   serv_addr.sin_family = AF_INET;
-   serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
-   serv_addr.sin_port = htons(atoi(argv[2]));
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+	serv_addr.sin_port = htons(atoi(argv[2]));
 
-   if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
-      error_handling("connect() error");
+	//connet
+	if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+		error_handling("connect() error");
 
-   close(sock);
+	//close
+	close(sock);
 
 	return 0;
+}
+
+//error check
+void error_handling(char *msg)
+{
+   fputs(msg, stderr);
+   fputc('\n', stderr);
+   exit(1);
 }
