6f42eace987e008c5d0a27103d9585658eee5044
양창엽 <ChangYeop-Yang@users.noreply.github.com>
2017-12-10 17:55:35 +0900

    Update Report, Server, Client
    Update Report, Server, Client
    2017.12.10. Source and Running File

--- Client.c
+++ Client.c
@@ -1,3 +1,9 @@
+/*
+ *file:server.c
+ *author:양창엽 양현아 우성연 왕신자
+ *datetime:2017_12_10 17:37
+ *description : Linux Multi-Thread Chatting Program.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h> 
@@ -6,57 +12,56 @@
 #include <sys/socket.h>
 #include <pthread.h>
 
-void error_handling(char * msg);
+#define BUF_SIZE 100
+#define NAME_SIZE 20
+
 void * send_msg(void * arg);
 void * recv_msg(void * arg);
+void error_handling(char * msg);
+
+char name[NAME_SIZE] = "[DEFAULT]";
+char msg[BUF_SIZE];
 
 int main(int argc, char *argv[])
 {
-	int sock;
-	struct sockaddr_in serv_addr;
-
-	pthread_t snd_thread, rcv_thread;
+   int sock;
+   struct sockaddr_in serv_addr;
+   pthread_t snd_thread, rcv_thread;
    void * thread_return;
+   if (argc != 3) {
+      printf("Usage : %s <IP> <port>\n", argv[0]);
+      exit(1);
+   }
 
-	if (argc != 3) {
-		printf("Usage : %s <IP> <port>\n", argv[0]);
-		exit(1);
-	}
+   sock = socket(PF_INET, SOCK_STREAM, 0);
 
-	//socket
-	sock = socket(PF_INET, SOCK_STREAM, 0);
+   memset(&serv_addr, 0, sizeof(serv_addr));
+   serv_addr.sin_family = AF_INET;
+   serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+   serv_addr.sin_port = htons(atoi(argv[2]));
 
-	memset(&serv_addr, 0, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
-	serv_addr.sin_port = htons(atoi(argv[2]));
+   if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+      error_handling("connect() error");
 
-	//connet
-	if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
-		error_handling("connect() error");
-
-	pthread_create(&snd_thread, NULL, send_msg, (void*)&sock);
+	/* Pthread Create & Join */
+   pthread_create(&snd_thread, NULL, send_msg, (void*)&sock);
    pthread_create(&rcv_thread, NULL, recv_msg, (void*)&sock);
    pthread_join(snd_thread, &thread_return);
    pthread_join(rcv_thread, &thread_return);
-
-	//close
-	close(sock);
-
-	return 0;
+   close(sock);
+   return 0;
 }
 
-// send thread main
-void * send_msg(void * arg)   
+void * send_msg(void * arg)   // send thread main
 {
    int sock = *((int*)arg);
    char name_msg[NAME_SIZE + BUF_SIZE];
-
-   while(1)
+   while (1)
    {
       fgets(msg, BUF_SIZE, stdin);
-      if (!strcmp(msg, "q\n"))
+      if (!strcmp(msg, "q\n") || !strcmp(msg, "Q\n"))
       {
+         close(sock);
          exit(0);
       }
       sprintf(name_msg,  msg);
@@ -65,10 +70,22 @@ void * send_msg(void * arg)
    return NULL;
 }
 
-// read thread main
-void * recv_msg(void * arg){}
+void * recv_msg(void * arg)   // read thread main
+{
+   int sock = *((int*)arg);
+   char name_msg[NAME_SIZE + BUF_SIZE];
+   int str_len;
+   while (1)
+   {
+      str_len = read(sock, name_msg, NAME_SIZE + BUF_SIZE - 1);
+      if (str_len == -1)
+         return (void*)-1;
+      name_msg[str_len] = 0;
+      fputs(name_msg, stdout);
+   }
+   return NULL;
+}
 
-//error check
 void error_handling(char *msg)
 {
    fputs(msg, stderr);
